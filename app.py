9aaa3654c17772c0d36963adde90e4158a46967f
Claire <ClaireOzzz@users.noreply.huggingface.co>
2023-12-05 12:34:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -86,8 +86,59 @@ min-height: 60px;
 }
 """
 
+theme = gr.themes.Soft(
+    primary_hue="teal",
+    secondary_hue="gray",
+).set(
+    body_text_color_dark='*neutral_800',
+    background_fill_primary_dark='*neutral_50',
+    background_fill_secondary_dark='*neutral_50',
+    border_color_accent_dark='*primary_300',
+    border_color_primary_dark='*neutral_200',
+    color_accent_soft_dark='*neutral_50',
+    link_text_color_dark='*secondary_600',
+    link_text_color_active_dark='*secondary_600',
+    link_text_color_hover_dark='*secondary_700',
+    link_text_color_visited_dark='*secondary_500',
+    code_background_fill_dark='*neutral_100',
+    shadow_spread_dark='6px',
+    block_background_fill_dark='white',
+    block_label_background_fill_dark='*primary_100',
+    block_label_text_color_dark='*primary_500',
+    block_title_text_color_dark='*primary_500',
+    checkbox_background_color_dark='*background_fill_primary',
+    checkbox_background_color_selected_dark='*primary_600',
+    checkbox_border_color_dark='*neutral_100',
+    checkbox_border_color_focus_dark='*primary_500',
+    checkbox_border_color_hover_dark='*neutral_300',
+    checkbox_border_color_selected_dark='*primary_600',
+    checkbox_label_background_fill_selected_dark='*primary_500',
+    checkbox_label_text_color_selected_dark='white',
+    error_background_fill_dark='#fef2f2',
+    error_border_color_dark='#b91c1c',
+    error_text_color_dark='#b91c1c',
+    error_icon_color_dark='#b91c1c',
+    input_background_fill_dark='white',
+    input_background_fill_focus_dark='*secondary_500',
+    input_border_color_dark='*neutral_50',
+    input_border_color_focus_dark='*secondary_300',
+    input_placeholder_color_dark='*neutral_400',
+    slider_color_dark='*primary_500',
+    stat_background_fill_dark='*primary_300',
+    table_border_color_dark='*neutral_300',
+    table_even_background_fill_dark='white',
+    table_odd_background_fill_dark='*neutral_50',
+    button_primary_background_fill_dark='*primary_500',
+    button_primary_background_fill_hover_dark='*primary_400',
+    button_primary_border_color_dark='*primary_00',
+    button_secondary_background_fill_dark='whiite',
+    button_secondary_background_fill_hover_dark='*neutral_100',
+    button_secondary_border_color_dark='*neutral_200',
+    button_secondary_text_color_dark='*neutral_800'
+)
 
-with gr.Blocks(css=css) as demo:
+
+with gr.Blocks(theme=theme, css=css) as demo:
 
     gr.Markdown("SUTD x SUNS Shop Design Generator")
     with gr.Tab("Training"):
